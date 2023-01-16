27e3e397c31d0da72a969de10f9e8aaa8271439d
Albert Urbina <johan50693@gmail.com>
2023-01-15 21:41:30 -0400

    Se agrega validacion de parametros  a las peticiones de la rutas del torneo

--- routes/tournament.js
+++ routes/tournament.js
@@ -5,6 +5,7 @@
 
 
 import { Router } from 'express'
+import { check } from 'express-validator';
 import { createTournament, deleteTournament, getTournament, listTournaments, updateTournament } from '../controller/index.js';
 import { validarJWT, validarCampos } from '../middlewares/index.js';
 
@@ -13,8 +14,22 @@ const router= Router()
 //  Asignar middleware a todas las rutas
 router.use(validarJWT);
 
-router.post('/create',validarCampos, createTournament)
-router.put('/:id',validarCampos, updateTournament)
+router.post('/create',
+  [
+    check('name', 'El nombre es un campo obligatorio').not().isEmpty(),
+    check('description', 'La descripción es un campo obligatoria').not().isEmpty(),
+    check('exact_marker', 'El marcador exacto es un campo obligatorio').not().isEmpty(),
+    check('winner_selection', 'La seleccion del ganador es un campo obligatoria').not().isEmpty(),
+    check('goals_of_a_team', 'Los goles de un equipo es un ccampo obligatorio').not().isEmpty(),
+    check('goals_difference', 'La diferencia de goles es un ccampo obligatorio').not().isEmpty(),
+    validarCampos
+  ], createTournament)
+router.put('/:id',
+  [
+    check('name', 'El nombre es un campo obligatorio').not().isEmpty(),
+    check('description', 'La descripción es un campo obligatoria').not().isEmpty(),
+    validarCampos
+  ], updateTournament)
 router.get('/list',validarCampos, listTournaments)
 router.get('/:id',validarCampos, getTournament)
 router.delete('/:id',validarCampos, deleteTournament)
