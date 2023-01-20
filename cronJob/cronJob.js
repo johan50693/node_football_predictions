30637439a71c6b3a7f6bbd45aa99ffdbd787ae13
Albert Urbina <johan50693@gmail.com>
2023-01-20 12:34:29 -0400

    Se elimina ciclo con rango de fechas para la tarea de actualizacion de resultados diarios

--- cronJob/cronJob.js
+++ cronJob/cronJob.js
@@ -18,13 +18,11 @@ export const cronEveryDayFirst = () => {
 
   cron.schedule('0 6 * * 0-7', async () => {
     console.log('running a task update matches At 06:00 on every day-of-week from Sunday through Sunday.');
-    for (let i = -1; i < 7; i++) {
-      let today = new Date()
-      today.setDate(today.getDate() + i)
-      console.log(today)
-      const newDate= today.toISOString().split("T")[0].split('-')
-      await updateScraping(newDate[0],newDate[1],newDate[2])
-    }
+  
+    let today = new Date()
+    const newDate= today.toISOString().split("T")[0].split('-')
+    await updateScraping(newDate[0],newDate[1],newDate[2])
+    
   });
 
 }
@@ -33,13 +31,11 @@ export const cronEveryDaySecond = () => {
 
   cron.schedule('0 12 * * 0-7', async () => {
     console.log('running a task update matches At 12:00 on every day-of-week from Sunday through Sunday.');
-    for (let i = -1; i < 7; i++) {
-      let today = new Date()
-      today.setDate(today.getDate() + i)
-      console.log(today)
-      const newDate= today.toISOString().split("T")[0].split('-')
-      await updateScraping(newDate[0],newDate[1],newDate[2])
-    }
+    
+    let today = new Date()
+    const newDate= today.toISOString().split("T")[0].split('-')
+    await updateScraping(newDate[0],newDate[1],newDate[2])
+
   });
   
 }
@@ -48,13 +44,11 @@ export const cronEveryDayThird = () => {
 
   cron.schedule('0 17 * * 0-7', async () => {
     console.log('running a task update matches At 17:00 on every day-of-week from Sunday through Sunday.');
-    for (let i = -1; i < 7; i++) {
-      let today = new Date()
-      today.setDate(today.getDate() + i)
-      console.log(today)
-      const newDate= today.toISOString().split("T")[0].split('-')
-      await updateScraping(newDate[0],newDate[1],newDate[2])
-    }
+    
+    let today = new Date()
+    const newDate= today.toISOString().split("T")[0].split('-')
+    await updateScraping(newDate[0],newDate[1],newDate[2])
+
   });
   
 }
